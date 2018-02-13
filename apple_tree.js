e37156cd67635f485ef8283c9aaab5b2baecf16d
WikaSilo <wikanyaa@gmail.com>
2018-02-13 12:07:25 +0700

    Done

---
+++ apple_tree.js
@@ -0,0 +1,12 @@
+class AppleTree {
+  constructor(name, age, height, matureAge, healthyStatus) {
+    this._name = name
+    this._age = age
+    this._height = height
+    this._matureAge = matureAge
+    this._healthyStatus = healthyStatus
+    this._deadAge = 6
+  }
+}
+
+module.exports = AppleTree
---
+++ mango_tree.js
@@ -0,0 +1,12 @@
+class MangoTree {
+  constructor(name, age, height, matureAge, healthyStatus) {
+    this._name = name
+    this._age = age
+    this._height = height
+    this._matureAge = matureAge
+    this._healthyStatus = healthyStatus
+    this._deadAge = 4
+  }
+}
+
+module.exports = MangoTree
