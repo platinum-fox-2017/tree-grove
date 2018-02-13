48dc03da1588bc47faddadfb45de91fbc4e2fc95
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-13 10:47:14 +0700

    Release 0: Create a TreeGrove Class

---
+++ apple_tree.js
@@ -0,0 +1,12 @@
+class AppleTree {
+	constructor(name, age, height, mature_age, healthyStatus) {
+		this._name = name;
+		this._age = age;
+		this._height = height;
+		this._mature_age = mature_age;
+		this._healthyStatus = healthyStatus;
+		this._max_age = 5;
+	}
+}
+
+module.exports = AppleTree
---
+++ mango_tree.js
@@ -0,0 +1,12 @@
+class MangoTree {
+	constructor(name, age, height, mature_age, healthyStatus) {
+		this._name = name;
+		this._age = age;
+		this._height = height;
+		this._mature_age = mature_age;
+		this._healthyStatus = healthyStatus;
+		this._max_age = 6;
+	}
+}
+
+module.exports = MangoTree
